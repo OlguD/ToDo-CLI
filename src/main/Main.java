16866812fc81e8444f78d797b810054b1d39c4c4
OlguD <olgudegirmenci34@gmail.com>
2024-11-12 23:29:54 +0200

    to-do tested

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,8 +1,20 @@
 package ToDo.src.main;
 
-
-import src.ToDo;
+import ToDo.src.main.TaskManager;
 
 public class Main {
-    ToDo todo = new Todo();
+
+    TaskManager taskManager = new TaskManager();
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.taskManager.addTask("Task 1", "This is task 1");
+        main.taskManager.addTask("Task 2", "This is task 2");
+        main.taskManager.addTask("Task 3", "This is task 3");
+        main.taskManager.addTask("Task 4", "This is task 4");
+        main.taskManager.addTask("Task 5", "This is task 5");
+        main.taskManager.saveTasksToFile();
+
+        main.taskManager.displayTask(); // Parametre geçmeyin
+    }
 }
